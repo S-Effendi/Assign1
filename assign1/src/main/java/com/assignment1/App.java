0d49931ace2441213f6186d0b79b763273843f95
S-Effendi <s.effendi93@gmail.com>
2017-03-12 04:18:46 +0200

    Slave2

--- assign1/src/main/java/com/assignment1/App.java
+++ assign1/src/main/java/com/assignment1/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( " Introduction to git " );
-	System.out.println( "Recording new slave" );
+	System.out.println( " Recording new slave " );
+	System.out.println( " Creating 2nd slave " );
     }
 }
